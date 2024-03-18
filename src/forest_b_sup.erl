b5b5f8f6076294e60b4bcfead72595ce5acb1537
Benjamin Jenson <benjamin.jenson11@yahoo.com>
2024-03-18 16:27:11 -0600

    bringing in line with current version

--- src/forest_b_sup.erl
+++ src/forest_b_sup.erl
@@ -27,8 +27,8 @@ start_link() ->
 %%                  modules => modules()}   % optional
 init([]) ->
     SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
-                 period => 1},
+                 intensity => 1,
+                 period => 10},
     ChildSpecs = [child(customer_request_server,worker),child(update_price_server,worker)],
     {ok, {SupFlags, ChildSpecs}}.
 
